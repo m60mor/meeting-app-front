64ee7ee2f95e9343b5a0752ac0abf30add216791
m60mor <m60mor@gmail.com>
2023-09-19 22:17:48 +0200

    Creating basic folder structure

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <div>
+            Sample Text
+        </div>
+    )
+}
---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,8 @@
+import 'homePage.css';
+import Navbar from '../../components/navbar/Navbar';
+
+export default function HomePage() {
+    return (
+        <Navbar />
+    )
+}
